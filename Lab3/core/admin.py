33e1bfae06d32dd17439c8c318e376a7c15a41c3
Alek Jarmov <alek.jarmov@gmail.com>
2023-05-19 22:06:48 +0200

    Lab 3 second part

--- Lab3/core/admin.py
+++ Lab3/core/admin.py
@@ -1,3 +1,106 @@
+from typing import Union, Optional
+
 from django.contrib import admin
+from .models import Blog, Comment, BlockList, File, BlogUser
+from rangefilter.filters import DateTimeRangeFilter, DateRangeFilterBuilder
+
 
 # Register your models here.
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_filter = (
+        ("created_at", DateRangeFilterBuilder(title="Created at")),
+    )
+    list_display = ('title', 'author')
+    search_fields = ('title', 'content')
+
+    def has_view_permission(self, request, obj: Optional[Blog] = None):
+        if obj is None:
+            return True
+
+        if request.user not in obj.author.blocked_users.all():
+            return True
+
+        if not BlockList.objects.filter(user=obj.author.user, blocked_user=request.user).exists():
+            return True  # alternative way to do it
+
+        return False
+
+    def has_add_permission(self, request, obj: Optional[Blog] = None):
+        return True
+
+    def has_change_permission(self, request, obj: Optional[Blog] = None):
+        if obj is None:
+            return False
+
+        if request.user.is_superuser or request.user == obj.author.user:
+            return True
+
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is None:
+            return False
+
+        if request.user == obj.author.user:
+            return True
+
+        if request.user.is_superuser:
+            return True
+
+        return False
+
+
+class CommentAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return True
+
+    def has_delete_permission(self, request, obj: Optional[Comment] = None):
+        return self.edit_delete_permission(request, obj)
+
+    def has_change_permission(self, request, obj: Optional[Comment] = None):
+        return CommentAdmin.edit_delete_permission(request, obj)
+
+    @staticmethod
+    def edit_delete_permission(request, obj: Optional[Comment] = None):
+        if obj is None:
+            return False
+
+        if request.user == obj.author.user:
+            return True
+
+        if request.user.is_superuser:
+            return True
+
+        return False
+
+
+class PostUserAdmin(admin.ModelAdmin):
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    def has_add_permission(self, request):
+        return True
+
+    def has_change_permission(self, request, obj: Optional[BlogUser] = None):
+        if obj is None:
+            return False
+
+        if request.user == obj.user:
+            return True
+
+        if request.user.is_superuser:
+            return True
+
+        return False
+
+
+class FileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(BlogUser, PostUserAdmin)
+admin.site.register(File, FileAdmin)
--- Lab3/core/models.py
+++ Lab3/core/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class PostUser(models.Model):
+class BlogUser(models.Model):
     user: User = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
 
@@ -13,7 +13,7 @@ class PostUser(models.Model):
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(PostUser, on_delete=models.CASCADE)
+    author = models.ForeignKey(BlogUser, on_delete=models.CASCADE)
     content = models.TextField()
     files = models.FileField(upload_to='files/', blank=True)
     created_at = models.DateField(auto_now_add=True)
@@ -21,7 +21,17 @@ class Blog(models.Model):
 
 
 class Comment(models.Model):
-    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    user = models.ForeignKey(PostUser, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="blog")
+    author = models.ForeignKey(BlogUser, on_delete=models.CASCADE, related_name="author")
     content = models.TextField()
     created_at = models.DateField(auto_now_add=True)
+
+
+class BlockList(models.Model):
+    user = models.ForeignKey(BlogUser, on_delete=models.CASCADE, related_name="blocker_user")
+    blocked_user = models.ForeignKey(BlogUser, on_delete=models.CASCADE, related_name='blocked_users')
+
+
+class File(models.Model):
+    file = models.FileField(upload_to='files/', blank=True)
+    foreign_key = models.ForeignKey(Blog, on_delete=models.CASCADE)
